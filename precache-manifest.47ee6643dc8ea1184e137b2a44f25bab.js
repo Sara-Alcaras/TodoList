671c6c2ed887514c90a6ea7eee6ba9edab93349e
Sara-Alcaras <saraalcaras401@gmail.com>
2020-05-14 16:17:20 -0300

    Updates

---
+++ precache-manifest.47ee6643dc8ea1184e137b2a44f25bab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e02f4cf0d28e5fae6b116cd3a5783feb",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "b0f97723b04b3650196f",
+    "url": "/TodoList/static/css/main.185aa0af.chunk.css"
+  },
+  {
+    "revision": "cce913ef2e044a1e27f7",
+    "url": "/TodoList/static/js/2.153ade1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoList/static/js/2.153ade1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0f97723b04b3650196f",
+    "url": "/TodoList/static/js/main.365c9e66.chunk.js"
+  },
+  {
+    "revision": "86378ab6fba0c65b9af6",
+    "url": "/TodoList/static/js/runtime-main.66968659.js"
+  }
+]);
